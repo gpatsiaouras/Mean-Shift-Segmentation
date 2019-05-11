9d746464af16948db3b9cef05351d8ea133eef79
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2019-05-11 20:38:37 +0200

    All good

--- src/main.py
+++ src/main.py
@@ -9,29 +9,29 @@ class Segmentation:
     def __init__(self, data, radius):
         self.data = data
         self.radius = radius
-        self.previous_data_point = None
         self.conversion_threshold = self.radius/2
         self.threshold = 0.01
         self.peaks = []
+        self.labels = np.zeros(data.shape[1])
 
-    def find_peak(self, subdata):
-        mean_data_point = np.mean(subdata, axis=1)
-        if self.previous_data_point is None or not self.is_converged(mean_data_point):
-            self.previous_data_point = mean_data_point
-            new_subdata = self.get_sphere(mean_data_point)
-            return self.find_peak(new_subdata)
-        else:
-            return mean_data_point
+    def find_peak(self, previous_data_point):
+        neighbors = self.get_data_in_radius_from_point(previous_data_point)
+        mean_data_point = np.mean(neighbors, axis=1)
+        while not self.converged(mean_data_point, previous_data_point):
+            previous_data_point = mean_data_point
+            neighbors = self.get_data_in_radius_from_point(mean_data_point)
+            mean_data_point = np.mean(neighbors, axis=1)
+
+        return mean_data_point
 
     def mean_shift(self):
         for i in range(self.data.shape[1]):
-            subdata = self.get_sphere(self.data[:, i])
-            peak = self.find_peak(subdata)
-            self.add_peak_to_list(peak)
+            peak = self.find_peak(self.data[:, i])
+            self.labels[i] = self.get_label_for_point(peak)
             sys.stdout.write("\rPoint: {0}, Peaks: {1}".format(i, len(self.peaks)))
             sys.stdout.flush()
 
-    def get_sphere(self, cluster_point):
+    def get_data_in_radius_from_point(self, cluster_point):
         subdata = []
         for i in range(self.data.shape[1]):
             if self.get_euclidean_distance(cluster_point, self.data[:, i]) < self.radius:
@@ -44,27 +44,24 @@ class Segmentation:
             (x2 - x1) ** 2
         ))
 
-    def is_converged(self, mean_data_point):
-        if self.previous_data_point is not None:
-            return (np.abs(mean_data_point - self.previous_data_point) < self.conversion_threshold).all()
-        return False
+    def converged(self, mean_data_point, previous_data_point):
+        return self.get_euclidean_distance(mean_data_point, previous_data_point) < self.threshold
 
-    def add_peak_to_list(self, new_peak):
-        found_similar_peak = False
-        for peak in self.peaks:
-            diff = np.abs(new_peak - peak)
-            if (diff < self.threshold).all():
-                found_similar_peak = True
-                break
+    def get_label_for_point(self, new_peak):
+        for peak_idx in range(len(self.peaks)):
+            if (np.abs(new_peak - self.peaks[peak_idx]) < self.conversion_threshold).all():
+                return peak_idx
 
-        if not found_similar_peak:
-            self.peaks.append(new_peak)
+        self.peaks.append(new_peak)
+
+        return len(self.peaks) - 1
 
 
 if __name__ == "__main__":
     # Read data
     data = np.array(scipy.io.loadmat('pts.mat')['data'])
-
+    # np.random.shuffle(data.T)
+    # data = data[:, :100]
     image_seg = Segmentation(data, 2)
     image_seg.mean_shift()
 
@@ -74,3 +71,5 @@ if __name__ == "__main__":
     peaks = np.array(image_seg.peaks).T
     ax.scatter(peaks[0], peaks[1], peaks[2])
     plt.show()
+
+    # print(image_seg.labels)
