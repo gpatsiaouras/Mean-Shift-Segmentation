13124fb54964bf8b9406bb12495fb9256e0b52ef
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2019-05-12 12:00:03 +0200

    Fixed slow execution

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import numpy as np
 import scipy.io
 import matplotlib.pyplot as plt
@@ -9,43 +10,39 @@ class Segmentation:
     def __init__(self, data, radius):
         self.data = data
         self.radius = radius
-        self.conversion_threshold = self.radius/2
+        self.conversion_threshold = self.radius / 2
         self.threshold = 0.01
         self.peaks = []
         self.labels = np.zeros(data.shape[1])
 
     def find_peak(self, previous_data_point):
         neighbors = self.get_data_in_radius_from_point(previous_data_point)
-        mean_data_point = np.mean(neighbors, axis=1)
+        mean_data_point = np.mean(neighbors, axis=1).reshape(self.data.shape[0], 1)
         while not self.converged(mean_data_point, previous_data_point):
             previous_data_point = mean_data_point
             neighbors = self.get_data_in_radius_from_point(mean_data_point)
-            mean_data_point = np.mean(neighbors, axis=1)
+            mean_data_point = np.mean(neighbors, axis=1).reshape(self.data.shape[0], 1)
 
         return mean_data_point
 
     def mean_shift(self):
+        start_time = time.time()
         for i in range(self.data.shape[1]):
-            peak = self.find_peak(self.data[:, i])
+            peak = self.find_peak(self.data[:, i].reshape(self.data.shape[0], 1))
             self.labels[i] = self.get_label_for_point(peak)
             sys.stdout.write("\rPoint: {0}, Peaks: {1}".format(i, len(self.peaks)))
             sys.stdout.flush()
 
+        sys.stdout.write("\rTime took: {0:.2f} seconds".format(time.time() - start_time) * 1000)
+        sys.stdout.flush()
+
     def get_data_in_radius_from_point(self, cluster_point):
-        subdata = []
-        for i in range(self.data.shape[1]):
-            if self.get_euclidean_distance(cluster_point, self.data[:, i]) < self.radius:
-                subdata.append(self.data[:, i])
-
-        return np.array(subdata).T
-
-    def get_euclidean_distance(self, x1, x2):
-        return np.sqrt(np.sum(
-            (x2 - x1) ** 2
-        ))
+        condition = np.linalg.norm(cluster_point - self.data, axis=0) < self.radius
+        extracted = np.extract(np.tile(condition, (3, 1)), self.data)
+        return extracted.reshape(3, extracted.shape[0] // 3)
 
     def converged(self, mean_data_point, previous_data_point):
-        return self.get_euclidean_distance(mean_data_point, previous_data_point) < self.threshold
+        return np.linalg.norm(mean_data_point - previous_data_point) < self.threshold
 
     def get_label_for_point(self, new_peak):
         for peak_idx in range(len(self.peaks)):
@@ -68,7 +65,7 @@ if __name__ == "__main__":
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(data[0], data[1], data[2])
-    peaks = np.array(image_seg.peaks).T
+    peaks = np.array(image_seg.peaks).reshape(2, 3).T
     ax.scatter(peaks[0], peaks[1], peaks[2])
     plt.show()
 
